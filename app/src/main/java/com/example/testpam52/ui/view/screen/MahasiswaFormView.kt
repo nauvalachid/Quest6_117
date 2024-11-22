1f94bae445e6d57678d475b297b85675864aba17
nauvalachid <nauvalachid@gmail.com>
2024-11-22 20:10:57 +0700

    Menambahkan composable 'MahasiwaFormView' untuk menampilkan form input data mahasiswa dengan aksi submit dan kembali.

--- app/src/main/java/com/example/testpam52/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/testpam52/ui/view/screen/MahasiswaFormView.kt
@@ -1,2 +1,11 @@
 package com.example.testpam52.ui.view.screen
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun MahasiwaFormView(
+    onSubmitButtonClickned: (MutableList<String>)->Unit,
+    onBackButtonClickned:() -> Unit
+) {
