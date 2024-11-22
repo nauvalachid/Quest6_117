60be6fc808b89ad4ef20c31fa63e1f2035b4f24e
nauvalachid <nauvalachid@gmail.com>
2024-11-22 20:12:03 +0700

    Menyimpan dan mengelola state untuk input nama, NIM, dan email mahasiswa menggunakan mutableStateOf, serta membuat listData untuk menyimpan data tersebut.

--- app/src/main/java/com/example/testpam52/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/testpam52/ui/view/screen/MahasiswaFormView.kt
@@ -1,6 +1,10 @@
 package com.example.testpam52.ui.view.screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 
 @Preview
@@ -9,3 +13,9 @@ fun MahasiwaFormView(
     onSubmitButtonClickned: (MutableList<String>)->Unit,
     onBackButtonClickned:() -> Unit
 ) {
+    var nama by remember { mutableStateOf("") }
+    var nim by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+
+    var listData: MutableList<String> = mutableListOf(nim,nama,email)
+}
