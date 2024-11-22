34345ad1ee336a0663e09cc95cb5f245152158b9
nauvalachid <nauvalachid@gmail.com>
2024-11-22 19:57:27 +0700

    Menambahkan fungsi @Composable 'MahasiswaApp' untuk mengatur UI utama aplikasi dengan dukungan ViewModel dan navigasi

--- app/src/main/java/com/example/testpam52/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/testpam52/navigation/PengelolaHalaman.kt
@@ -1,8 +1,20 @@
 package com.example.testpam52.navigation
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
 enum class Halaman {
     Splash,
     Mahasiswa,
     MataKuliah,
     Tampildata
 }
+
+@Composable
+fun MahasiswaApp(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    rencanaStudyViewModel: RencanaStudyViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+){
