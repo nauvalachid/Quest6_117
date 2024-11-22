e13cccaebf7370e575974c811b78e28bda7035b1
nauvalachid <nauvalachid@gmail.com>
2024-11-22 20:31:13 +0700

    Mengimport class RencanaStudyviewmodel

--- app/src/main/java/com/example/testpam52/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/testpam52/navigation/PengelolaHalaman.kt
@@ -2,6 +2,7 @@ package com.example.testpam52.navigation
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
@@ -11,6 +12,7 @@ import androidx.navigation.compose.rememberNavController
 import com.example.testpam52.ui.view.screen.MahasiwaFormView
 import com.example.testpam52.ui.view.screen.SplashView
 import com.example.testpam52.ui.view.viewmodel.MahasiswaViewModel
+import com.example.testpam52.ui.view.viewmodel.RencanaStudyViewModel
 
 enum class Halaman {
     Splash,
